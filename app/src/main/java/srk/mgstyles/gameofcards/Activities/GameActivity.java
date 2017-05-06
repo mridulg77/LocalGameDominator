3d071d2c421cd25ac8e7584d593657dc6891636e
mridulg77 <x.mridul.y@gmail.com>
2017-05-06 09:57:38 +0530

    second

--- app/src/main/java/srk/mgstyles/gameofcards/Activities/GameActivity.java
+++ app/src/main/java/srk/mgstyles/gameofcards/Activities/GameActivity.java
@@ -15,7 +15,7 @@ public class GameActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.first_game);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
